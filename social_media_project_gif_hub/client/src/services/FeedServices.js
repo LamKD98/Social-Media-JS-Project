53e333eebc6fed3a4dc941fc7591aaad46cc4ddb
Codeclan Student <codeclanstudent@Codeclans-MacBook-Pro.local>
2023-05-21 11:52:45 +0100

    Feed Service

---
+++ social_media_project_gif_hub/client/src/services/FeedServices.js
@@ -0,0 +1,129 @@
+const usersURL = 'http://localhost:9000/api/users/';
+const postsURL = 'http://localhost:9001/api/posts/';
+const commentsURL = 'http://localhost:9002/api/comments/';
+
+const FeedServices = {
+    // Users
+    getUsers() {
+        return fetch(usersURL)
+          .then(res => {
+            if (!res.ok) {
+              throw new Error(res.status);
+            }
+            return res.json();
+          })
+          .catch(err => console.error(err));
+      },
+      
+
+    addUser(user) {
+        return fetch(usersURL, {
+        method: 'POST',
+        body: JSON.stringify(user),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+        })
+        .then(res => res.json());
+    },
+
+    updateUser(user) {
+        return fetch(usersURL + user._id, {
+        method: 'PUT',
+        body: JSON.stringify(user),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+        })
+        .then(res => res.json());
+    },
+
+    deleteUser(id) {
+        return fetch(usersURL + id, {
+        method: 'DELETE'
+        });
+    },
+
+    // Posts
+    getPosts() {
+        return fetch(postsURL)
+        .then(res => {
+        if (!res.ok) {
+            throw new Error(res.status);
+        }
+        return res.json();
+        })
+        .catch(err => console.error(err));
+    },
+
+    addPost(post) {
+        return fetch(postsURL, {
+        method: 'POST',
+        body: JSON.stringify(post),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+        })
+        .then(res => res.json());
+    },
+
+    updatePost(post) {
+        return fetch(postsURL + post._id, {
+        method: 'PUT',
+        body: JSON.stringify(post),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+        })
+        .then(res => res.json());
+    },
+
+    deletePost(id) {
+        return fetch(postsURL + id, {
+        method: 'DELETE'
+        });
+    },
+
+    // Comments
+    getComments() {
+        return fetch(commentsURL)
+        .then(res => {
+        if (!res.ok) {
+            throw new Error(res.status);
+        }
+        return res.json();
+        })
+        .catch(err => console.error(err));
+    },
+
+    addComment(comment) {
+        return fetch(commentsURL, {
+        method: 'POST',
+        body: JSON.stringify(comment),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+        })
+        .then(res => res.json());
+    },
+
+    updateComment(comment) {
+        return fetch(commentsURL + comment._id, {
+        method: 'PUT',
+        body: JSON.stringify(comment),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+        })
+        .then(res => res.json());
+    },
+
+    deleteComment(id) {
+        return fetch(commentsURL + id, {
+        method: 'DELETE'
+        });
+    },
+};
+
+export default FeedServices;
+
