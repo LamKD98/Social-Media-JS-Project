64f9253f1e256f254aa17c240dcc4708c948c4fd
Codeclan Student <codeclanstudent@Codeclans-MacBook-Pro.local>
2023-05-23 09:26:53 +0100

    comment files

--- social_media_project_gif_hub/client/src/components/feed/PostCard.js
+++ social_media_project_gif_hub/client/src/components/feed/PostCard.js
@@ -1,42 +1,31 @@
 import React from 'react';
+import CommentForm from './CommentForm';
 import CommentCard from './CommentCard';
-import GifPicker, { TenorImage } from 'gif-picker-react';
 
-const PostCard = ({post, user, users, comments, comment}) => {
-
-
-    console.log(post.gifUrl)
+const PostCard = ({post, user, users, comments, onCommentCreate}) => {
 
     const commentCardNodes = () => {
         const postComments = comments.filter((comment) =>  post.postDate === comment.postDate)
-        console.log(postComments);
         const postCommentNodes = postComments.map((postComment, index) => {
             return (
                 <CommentCard
                     key={index}
                     comment={postComment}
-           
                 />
         )})
         return postCommentNodes
     }
 
-        
-    
-
     return (
         <div className='post-card'>
             <h2>{user.fname}:</h2>
             <p>{post.text}</p>
-
             <img
                 src={post.gifUrl}
                 alt="Selected GIF"
             />
-
             {commentCardNodes()}
-            {/* <CommentForm / >  */}
-
+            <CommentForm onCommentCreate={onCommentCreate} postId={post.postId}/>
         </div>
     );
 }
