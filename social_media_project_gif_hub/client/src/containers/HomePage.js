073e80d1a22ae6c748a46eee626f35b3da1edb59
saladinzero <117020385+saladinzero@users.noreply.github.com>
2023-05-25 14:48:04 +0100

    fixed extra router

--- social_media_project_gif_hub/client/src/containers/HomePage.js
+++ social_media_project_gif_hub/client/src/containers/HomePage.js
@@ -1,6 +1,6 @@
 import './Homepage.css'
 import React from 'react';
-import { BrowserRouter as Link} from 'react-router-dom';
+import {Link} from 'react-router-dom';
 
 
 const HomePage = ({loggedInUser}) => {
